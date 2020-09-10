e65e4cd0af83f8bd8f8668cccb3ddc87033ee845
NinFreakinJa <60324136+NinFreakinJa@users.noreply.github.com>
2020-09-10 17:19:58 -0400

    Update myList.py

--- myList.py
+++ myList.py
@@ -1,4 +1,5 @@
 import os
+import time
 path=os.getcwd()
 list=os.listdir(path)
 header="<h1>"+path+"</h1>"
@@ -10,3 +11,5 @@ for i in list:
     f.write("<p>"+i+"</p>")
 f.close()
 os.system("browse myList.html")
+time.sleep(3)
+os.system("rm myList.html")
