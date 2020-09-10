4e0a2d399dc0d86bef7d4455b9774d22d01198a0
NinFreakinJa <60324136+NinFreakinJa@users.noreply.github.com>
2020-09-10 17:10:18 -0400

    Update myList.py

--- myList.py
+++ myList.py
@@ -5,4 +5,8 @@ header="<h1>"+path+"</h1>"
 f = open("myList.html", "w")
 f.write(header+"\n")
 f.close()
+f = open("myList.html", "a")
+for i in list:
+    f.write("<p>"+i+"</p>")
+f.close()
 os.system("browse myList.html")
