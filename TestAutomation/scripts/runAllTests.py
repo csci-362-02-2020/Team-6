cb6daf1ed654d2ab332f7f9e7d774ab663431116
NinFreakinJa <60324136+NinFreakinJa@users.noreply.github.com>
2020-10-20 16:56:42 -0400

    testCases

--- TestAutomation/scripts/runAllTests.py
+++ TestAutomation/scripts/runAllTests.py
@@ -3,9 +3,11 @@ import os
 path=os.getcwd()
 os.system("javac -cp . /project/src/* /testcasesexecutables/*")
 testDirectory="/testCases"
-for root, dirs, files in os.walk("/testCases"):
+for root, dirs, files in os.walk(testDirectory):
   for name in files:
-    os.system(commandReturn(testDirectory+"/"+name))
+    os.system(commandReturn(testDirectory+"/"+name)+">>"+testDirectory+"/"+name)
 
 def commandReturn(fileNamePath):
-  
+  with fName = open(fileNamePath, 'r')
+    for 4,line in enumerate(fName):
+    
