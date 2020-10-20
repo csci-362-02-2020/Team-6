d5ac2c87e33c8dd69dfd07d1114e5f806372859a
NinFreakinJa <60324136+NinFreakinJa@users.noreply.github.com>
2020-10-20 16:45:19 -0400

    Combine line 3 and 4 of testCases

--- TestAutomation/scripts/runAllTests.py
+++ TestAutomation/scripts/runAllTests.py
@@ -1,4 +1,11 @@
+#Run from inside TestAutomation directory
 import os
 path=os.getcwd()
 os.system("javac -cp . /project/src/* /testcasesexecutables/*")
-testDirectory="/testCases/"
+testDirectory="/testCases"
+for root, dirs, files in os.walk("/testCases"):
+  for name in files:
+    os.system(commandReturn(testDirectory+"/"+name))
+
+def commandReturn(fileNamePath):
+  
