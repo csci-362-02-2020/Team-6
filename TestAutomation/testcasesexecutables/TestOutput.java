b5a2f4aee42e70056952f1a487864f177f46cb1e
NinFreakinJa <60324136+NinFreakinJa@users.noreply.github.com>
2020-10-27 16:02:41 -0400

    Update TestOutput.java

--- TestAutomation/testcasesexecutables/TestOutput.java
+++ TestAutomation/testcasesexecutables/TestOutput.java
@@ -10,18 +10,19 @@ public class TestOutput{
     public static void replaceLines(String filename,String newLine){
         try {
             StringBuffer inputBuffer = new StringBuffer();
-            Scanner sc=new Scanner(new File(filename));
+            Scanner sc=new Scanner(TestOutput.class.getResourceAsStream(filename));
             int count=0;
             while (sc.hasNextLine()&&count<5) {
                 inputBuffer.append(sc.nextLine()+System.lineSeparator());
                 count++;
             }
             sc.close();
-            FileWriter writer=new FileWriter(filename);
+            System.out.println(inputBuffer.toString());
+            FileWriter writer=new FileWriter(new File(TestOutput.class.getResource(filename).getFile()));
             writer.write(inputBuffer.toString()+"\n"+newLine);
             writer.close();
 
-        } catch (Exception e) {
+        } catch (FileNotFoundException e) {
             System.out.println("Problem reading file.");
         }
     }
