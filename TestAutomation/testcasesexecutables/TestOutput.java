16b81d2c5349a1f4df83aeabb4c2a12aca666b07
NinFreakinJa <60324136+NinFreakinJa@users.noreply.github.com>
2020-10-27 16:58:09 -0400

    Update TestOutput.java

--- TestAutomation/testcasesexecutables/TestOutput.java
+++ TestAutomation/testcasesexecutables/TestOutput.java
@@ -4,11 +4,14 @@ import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileOutputStream;
 
 public class TestOutput{
     
-    public static void replaceLines(String filename,String newLine){
-        try {
+    public static void replaceLines(String filename,String newLine) throws IOException{
+        /*try {
             StringBuffer inputBuffer = new StringBuffer();
             Scanner sc=new Scanner(TestOutput.class.getResourceAsStream(filename));
             int count=0;
@@ -24,6 +27,27 @@ public class TestOutput{
 
         } catch (FileNotFoundException e) {
             System.out.println("Problem reading file.");
+        }*/
+        try{
+        	BufferedReader file=new BufferedReader(new FileReader(filename));
+        	//StringBuffer inputBuffer=new StringBuffer();
+        	String line;
+        	String lines;
+        	int count=0;
+        	while ((line = file.readLine()) != null && count<5) {
+            		lines+=line;
+            		//inputBuffer.append('\n');
+            		count++;
+        	}
+        	file.close();
+        	//String inputStr = inputBuffer.toString();
+        	//FileOutputStream fileOut=new FileOutputStream(filename);
+        	FileWriter fileOut=new FileWriter(filename);
+        	fileOut.write(lines+newLine);
+        	fileOut.close();
         }
+        catch (Exception e) {
+        	System.out.println("Problem reading file.");
+    	}
     }
 }
