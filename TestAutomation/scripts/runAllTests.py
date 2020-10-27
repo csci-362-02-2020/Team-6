1f6f35ff1bba6461760cc68ee361d27ef304e6e6
NinFreakinJa <60324136+NinFreakinJa@users.noreply.github.com>
2020-10-27 16:33:20 -0400

    Update runAllTests.py

--- TestAutomation/scripts/runAllTests.py
+++ TestAutomation/scripts/runAllTests.py
@@ -16,9 +16,9 @@ path=os.getcwd()
 os.system("javac -cp . project/src/* testcasesexecutables/*")
 testDirectory="testCases"
 reportDirectory="reports"
-for root, dirs, files in os.walk("testCases"):
+for root, dirs, files in os.walk(testDirectory):
   for name in files:
-    os.system(commandReturn(testDirectory+"/"+name))
+    os.system(commandReturn(testDirectory+"/"+name)+" "+os.path.abspath(testDirectory+"/"+name))
 
 
     
