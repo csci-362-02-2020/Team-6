9016f00d6ee7c4955d8fd9a8cbe56beb90187019
NinFreakinJa <60324136+NinFreakinJa@users.noreply.github.com>
2020-11-03 18:22:44 -0500

    Update TestOutput.java

--- TestAutomation/testcasesexecutables/TestOutput.java
+++ TestAutomation/testcasesexecutables/TestOutput.java
@@ -3,10 +3,10 @@ import java.io.FileWriter;
 import java.io.BufferedReader;
 import java.io.FileReader;
 
-//This class is for writing the results for each test case
-//Arguments: OutputFile Output
+//This class is for writing the results for each test cas
 public class TestOutput{
-    //Method only replaces (or adds) the last 2 lines of the testCase file to display results
+	//Method only replaces (or adds) the last 2 lines of the testCase file to display results
+	//Arguments: OutputFile Output
     public static void replaceLines(String filename,String newLine){
         try{
         	BufferedReader file=new BufferedReader(new FileReader(filename));
