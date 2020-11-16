005a68c4a1dedb3c9d9a2e8ce646b3a2c744a566
NinFreakinJa <60324136+NinFreakinJa@users.noreply.github.com>
2020-11-16 16:16:55 -0500

    Update testhex2RGB.java

--- TestAutomation/testcasesexecutables/testhex2RGB.java
+++ TestAutomation/testcasesexecutables/testhex2RGB.java
@@ -2,14 +2,10 @@ package testcasesexecutables;
 import java.awt.Color;
 import project.src.ColorConverter;
 
-import java.awt.Color;
-import java.util.Random;
 public class testhex2RGB{
   //this is the main class used to be testing Tanaguru's hex2RBG where
   //@arg: colorStr
-  public static void main(String[] args){
-
-	  
+	public static void main(String[] args){
 	int[] colorNum = new int[3];
   	for (int i=0;i<3;i++) {
   		colorNum[i] = Integer.parseInt(args[i]);
@@ -18,27 +14,17 @@ public class testhex2RGB{
     Color color1 = new Color(colorNum[0], colorNum[1], colorNum[2]);
     String result = ColorConverter.rgb2Hex(color1);
     result = result.substring(1, result.length());
-		String expectedOutcome = args[3];
-		String outputFile=args[4];
-  	
-		//Test result output
-		String output=String.valueOf(result)+"\n";
-		if (expectedOutcome.contentEquals(result)){
+	String expectedOutcome = args[3];
+	String outputFile=args[4];  	
+	//Test result output
+	String output=String.valueOf(result)+"\n";
+	if (expectedOutcome.contentEquals(result)){
   		output+="Pass";
-
-  		
-  	}
-		
+  	}	
   	else{
   		output+="Fail";
-		}
-		TestOutput.replaceLines(outputFile,output);
-		//System.out.println(result);
-		
-
-
-
-    
-    
-  }
-  }
+	}
+	TestOutput.replaceLines(outputFile,output);
+	//System.out.println(result);
+    }
+}
