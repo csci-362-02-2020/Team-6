94310b2919963d4eb1e67ce91651113f0e669593
veloffs <veloffs@g.cofc.edu>
2020-11-16 14:33:16 -0500

    some test cases and driver for test

---
+++ TestAutomation/testcasesexecutables/testhex2RGB.java
@@ -0,0 +1,44 @@
+package testcasesexecutables;
+import java.awt.Color;
+import project.src.ColorConverter;
+
+import java.awt.Color;
+import java.util.Random;
+public class testhex2RGB{
+  //this is the main class used to be testing Tanaguru's hex2RBG where
+  //@arg: colorStr
+  public static void main(String[] args){
+
+	  
+	int[] colorNum = new int[3];
+  	for (int i=0;i<3;i++) {
+  		colorNum[i] = Integer.parseInt(args[i]);
+  		
+  	}
+    Color color1 = new Color(colorNum[0], colorNum[1], colorNum[2]);
+    String result = ColorConverter.rgb2Hex(color1);
+    result = result.substring(1, result.length());
+		String expectedOutcome = args[3];
+		String outputFile=args[4];
+  	
+		//Test result output
+		String output=String.valueOf(result)+"\n";
+		if (expectedOutcome.contentEquals(result)){
+  		output+="Pass";
+
+  		
+  	}
+		
+  	else{
+  		output+="Fail";
+		}
+		TestOutput.replaceLines(outputFile,output);
+		//System.out.println(result);
+		
+
+
+
+    
+    
+  }
+  }
