979d6192f313cbb654ff1714c5cf0fe3a35cf26c
Joe Mezera <mezeraj@g.cofc.ued>
2020-11-16 19:38:54 -0500

    Added comments to drivers

--- TestAutomation/testcasesexecutables/TestGetHue.java
+++ TestAutomation/testcasesexecutables/TestGetHue.java
@@ -6,17 +6,19 @@ import testcasesexecutables.TestOutput;
 public class TestGetHue {
 	
 	public static void main(String[] args) {
-		int[] colorNum = new int[3];
+	int[] colorNum = new int[3];
+	//turns the string arguments into integers in an array
     	for (int i=0;i<colorNum.length;i++) {
     		colorNum[i] = Integer.parseInt(args[i]);
     	}
-    	
+    	//creates the color using the parameters from
     	Color color1 = new Color(colorNum[0], colorNum[1], colorNum[2]);
-    	
+    	//puts the expected Outcome parameter into a usable float value 
     	float expectedOutcome = Float.parseFloat(args[3]);
-		String outputFile=args[4];
+	String outputFile=args[4];
+	//gets the results from the tested method and stores it in a variable
     	float hueValue = ColorConverter.getHue(color1) * 360;
-		//Test result output
+		//Test result output to HTML
 		String output=String.valueOf(hueValue)+"\n";
 		if (expectedOutcome == hueValue){
 				output+="Pass";
--- TestAutomation/testcasesexecutables/TestOffsetRGBColor.java
+++ TestAutomation/testcasesexecutables/TestOffsetRGBColor.java
@@ -7,17 +7,20 @@ public class TestOffsetRGBColor {
 
 	public static void main(String[] args) {
 		int[] colorNum = new int[9];
+		//takes the String parameters and turns them into ints and stores them in an array
 	   	for (int i=0;i<colorNum.length;i++) {
 	    	colorNum[i] = Integer.parseInt(args[i]);
 	   	}
-	    	
+	    	//creates the color to be changed
 	   	Color color1 = new Color(colorNum[0], colorNum[1], colorNum[2]);
-	   	
+	   	//adds the expected outcome results to an integer array
 	   	int[] expectedOutcome = new int[]{colorNum[6], colorNum[7], colorNum[8]};
 		String outputFile=args[9];
+		//changes the Color color1 by the offset amounts set in the test case
 	   	color1 = ColorConverter.offsetRgbColor(color1, colorNum[3], colorNum[4], colorNum[5]);
+	   	stores the results of the offset in an integer array
 	   	int[] result = new int[]{color1.getRed(), color1.getGreen(), color1.getBlue()};
-		//Test result output
+		//Test result output to HTML
 		String output=String.valueOf(color1.getRed()+" "+color1.getGreen()+" "+color1.getBlue())+"\n";
 		int counter=0;
 		for(int i=0;i<3;i++){
