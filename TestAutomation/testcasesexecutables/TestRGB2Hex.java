858cd23dbf7d7b0dfdcea88fbcecc1c1529e5c0d
veloffs <45014784+veloffs@users.noreply.github.com>
2020-11-23 18:14:04 -0500

    Update TestRGB2Hex.java

--- TestAutomation/testcasesexecutables/TestRGB2Hex.java
+++ TestAutomation/testcasesexecutables/TestRGB2Hex.java
@@ -16,7 +16,9 @@ public class TestRGB2Hex{
     //create new color object:
     Color color1 = new Color(colorNum[0], colorNum[1], colorNum[2]);
     String result = ColorConverter.rgb2Hex(color1);
-    //result had to start at index 1 (2nd position) because the # was giving issues when doing comparison:	
+    //result had to start at index 1 (2nd position) because the # was giving issues when doing comparison:
+   //fault: replace result = result.substring(1, result.length()); to result = result.substring(0,result.length());
+  //this will not return he ctorrect expectedOutcome.
     result = result.substring(1, result.length());
 	String expectedOutcome = args[3];
 	String outputFile=args[4];  	
